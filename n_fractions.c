d5e17345e8edc443a0ac8aff5b2a75602ad5be61
Piyush-0120 <65674133+Piyush-0120@users.noreply.github.com>
2021-02-11 13:27:30 +0530

    Activity10

--- n_fractions.c
+++ n_fractions.c
@@ -2,41 +2,65 @@
 #include <stdio.h>
 struct frac
 {
-    float a;
-    float b;
+    int a;
+    int b;
 };
 typedef struct frac Frac;
+int gcd(int,int);
+int lcm(int,int);
+Frac input();
+Frac compute(Frac,Frac);
+void output(Frac);
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+int lcm(int a,int b)
+{
+    return ((a*b)/gcd(a,b)); 
+}
 Frac input()
 {
     Frac f;
     printf("Enter numerator:");
-    scanf("%f",&f.a);
+    scanf("%d",&f.a);
     printf("Enter denominator:");
-    scanf("%f",&f.b);
+    scanf("%d",&f.b);
     return f;
 }
-float compute(Frac f)
+Frac compute(Frac res,Frac f)
 {
-    float res;
-    res=(f.a / f.b); 
-    return res;
+    Frac sum;
+    int l = lcm(res.b,f.b);
+    sum.a = res.a*(l/res.b)  + f.a*(l/f.b);
+    sum.b = l;
+    return sum;
 }
-void output(float res)
+void output(Frac res)
 {
-    printf("The answer is: %f",res);
+    int g=gcd(res.a,res.b);
+    res.a/=g;
+    res.b/=g;
+    printf("The simplified fraction is: %d/%d",res.a,res.b);
 }
 int main(void)
 {
     int n;
-    float sum=0.0;
-    Frac f;
+    Frac res,f;
     printf("Enter the value of n:");
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
         f=input();
-        sum+=compute(f);
+        if(i==0)
+            res=f;
+        else
+            res=compute(res,f);
     }
-    output(sum);
+    output(res);
     return 0;
 }
